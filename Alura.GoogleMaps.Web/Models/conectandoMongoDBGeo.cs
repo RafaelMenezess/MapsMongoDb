4f5675164bcbe5b56b6c00c4f9047eb942a46a20
Rafael Menezes <rafa.rcm@hotmail.com>
2024-02-17 16:11:31 -0300

    Criandao as classes de buscas de aeroporto

--- Alura.GoogleMaps.Web/Models/conectandoMongoDBGeo.cs
+++ Alura.GoogleMaps.Web/Models/conectandoMongoDBGeo.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿using MongoDB.Driver;
 
 namespace Alura.GoogleMaps.Web.Models
 {
     public class conectandoMongoDBGeo
     {
-        //XXX TRABALHE AQUI
-        // Crie a classe de conexão com o MongoDB
+        public const string STRING_DE_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "geo";
+        public const string NOME_DA_COLECAO = "airports";
 
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
 
+        static conectandoMongoDBGeo()
+        {
+            _client = new MongoClient(STRING_DE_CONEXAO);
+            _database = _client.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+        public IMongoCollection<Aeroporto> Airports
+        {
+            get { return _database.GetCollection<Aeroporto>(NOME_DA_COLECAO); }
+        }
     }
 }
